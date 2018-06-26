984b14b9ba7d90fd35a994ce323017e4399969f6
asiafrank <zhangxfdev@gmail.com>
2018-06-26 17:20:48 +0800

    modify directx note

--- cpp/directx12/Common/UploadBuffer.h
+++ cpp/directx12/Common/UploadBuffer.h
@@ -25,7 +25,7 @@ public:
             &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
             D3D12_HEAP_FLAG_NONE,
             &CD3DX12_RESOURCE_DESC::Buffer(mElementByteSize*elementCount),
-			D3D12_RESOURCE_STATE_GENERIC_READ,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
             nullptr,
             IID_PPV_ARGS(&mUploadBuffer)));
 
