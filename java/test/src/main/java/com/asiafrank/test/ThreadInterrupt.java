0a5434ee8fd7a81a7e7d83a9e63409bfed98a437
asiafrank <zhangxfdev@gmail.com>
2018-08-26 22:01:08 +0800

    add bittorrent note (2)

--- java/test/src/main/java/com/asiafrank/test/ThreadInterrupt.java
+++ java/test/src/main/java/com/asiafrank/test/ThreadInterrupt.java
@@ -1,5 +1,20 @@
 package com.asiafrank.test;
 
+/**
+ * Java里一个线程调用了Thread.interrupt()到底意味着什么？ - Intopass的回答 - 知乎
+ * https://www.zhihu.com/question/41048032/answer/89431513
+ *
+ * http://www.fanyilun.me/2016/11/19/Thread.interrupt()%E7%9B%B8%E5%85%B3%E6%BA%90%E7%A0%81%E5%88%86%E6%9E%90/
+ *
+ * 源码见： os_windows.cpp,os_posix.cpp,park.cpp  重要方法 os::PlatformEvent::unpark()
+ * 然后下面方法的 native 代码会抛出 InterruptedException (hotspot 源码搜索 “java_lang_InterruptedException()” 就明白了)
+ * @see     java.lang.Object#wait()
+ * @see     java.lang.Object#wait(long)
+ * @see     java.lang.Object#wait(long, int)
+ * @see     java.lang.Thread#sleep(long)
+ * @see     java.lang.Thread#interrupt()
+ * @see     java.lang.Thread#interrupted()
+ */
 public class ThreadInterrupt {
     public static void main(String[] args) throws Exception {
         Thread t = new Thread(()->{
