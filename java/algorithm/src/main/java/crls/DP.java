af111df5ccf268a7f17acf9f8339c3b09c85393c
asiafrank <zhangxfdev@gmail.com>
2019-01-23 17:01:52 +0800

    add LCS-LENGTH

--- java/algorithm/src/main/java/crls/DP.java
+++ java/algorithm/src/main/java/crls/DP.java
@@ -1,7 +1,9 @@
 package crls;
 
+import java.util.Arrays;
+
 /**
- * 动态规划
+ * 动态规划-P204
  *
  * @author zhangxf created at 1/21/2019.
  */
@@ -62,6 +64,66 @@ public class DP {
             printOptimalParens(s, i, s[i][j]);
             printOptimalParens(s, s[i][j]+1, j);
             System.out.print(")");
+        }
+    }
+
+    //---------------------------------------
+    // 公共最长子序列
+    //---------------------------------------
+
+    public static class LCSPair {
+        public LCSPair(char[][] b, int[][] c) {
+            this.b = b;
+            this.c = c;
+        }
+
+        char[][] b;
+        int[][] c;
+    }
+
+    public static LCSPair lcsLength(int[] x, int[] y) {
+        int m = x.length - 1;
+        int n = y.length - 1;
+        char[][] b = new char[m+1][n+1]; // 帮助构造最优解的路径表
+        int[][] c = new int[m+1][n+1]; // 对于 x[i] y[j] 的最长公共子序列长度为 c[i][j]
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (x[i] == y[j]) {
+                    c[i][j] = c(c, i-1, j-1) + 1;
+                    b[i][j] = '↖';
+                } else if (c(c, i-1, j) >= c(c, i, j-1)) { // z[k]!=x[i] z[k]=y[j] 的情况
+                    c[i][j] = c(c, i-1, j);
+                    b[i][j] = '↑';
+                } else { // z[k]=x[i] z[k]!=y[j] 的情况
+                    c[i][j] = c(c, i, j-1);
+                    b[i][j] = '←';
+                }
+            }
+        }
+        return new LCSPair(b, c);
+    }
+
+    /**
+     * 取消算法导论伪代码中对 c[i][0] 和 c[0][j] 的初始化，并且传入的 x，y 数组下表从 0 开始
+     */
+    private static int c(int[][] c, int i, int j) {
+        if (i < 0 || j < 0)
+            return 0;
+        return c[i][j];
+    }
+
+    public static void printLCS(char[][] b, int[] x, int i, int j) {
+        if (i < 0 || j < 0)
+            return;
+
+        if (b[i][j] == '↖') {
+            printLCS(b, x, i - 1, j - 1);
+            System.out.print(x[i]);
+        } else if (b[i][j] == '↑') {
+            printLCS(b, x, i - 1, j);
+        } else {
+            printLCS(b, x, i, j - 1);
         }
     }
 }
--- java/algorithm/src/test/java/crls/DPTest.java
+++ java/algorithm/src/test/java/crls/DPTest.java
@@ -17,7 +17,27 @@ public class DPTest {
         printArray(pair.s);
     }
 
+    @Test
+    public void lcsLengthTest() {
+        int[] x = {'A', 'B', 'C', 'B', 'D', 'A', 'B'};
+        int[] y = {'B', 'D', 'C', 'A', 'B', 'A'};
+        DP.LCSPair pair = DP.lcsLength(x, y);
+        printArray(pair.c);
+        printArray(pair.b);
+        DP.printLCS(pair.b, x, x.length - 1, y.length - 1);
+        System.out.println();
+    }
+
     private void printArray(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void printArray(char[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 System.out.print(a[i][j] + " ");
