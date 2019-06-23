7392190ffa4cf579e0b435f668a18054a3385cbb
asiafrank <zhangxfdev@gmail.com>
2019-06-23 22:19:17 +0800

    add minigrep

--- rust/hello_cargo/src/main.rs
+++ rust/hello_cargo/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let r;
+    {
+        let x = 5;
+        r = x;
+    }
+
+    println!("r: {}", r);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
