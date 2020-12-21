2068c16c8fe32e0b2d7265a370e9a6eda0d02991
asiafrank <zhangxfdev@gmail.com>
2020-12-21 23:46:50 +0800

    题目

---
+++ java/algorithm/src/main/java/coursex/C03PartitionQuickSort.java
@@ -0,0 +1,22 @@
+package coursex;
+
+/**
+ * 快排
+ */
+public class C03PartitionQuickSort {
+
+    //
+    // TODO: 利用 partition 求第 k 大的数
+
+    /**
+     * TODO：荷兰国旗问题
+     * 将 arr[l..r] 分成三个部分：
+     * 小于 arr[r] 的
+     * 等于 arr[r] 的
+     * 大于 arr[r] 的
+     * 返回等于 arr[r] 的左边界和右边界。
+     */
+    public static int[] netherLandsFlag(int[] arr, int l, int r) {
+        return new int[]{};
+    }
+}
