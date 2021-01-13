b5a803ea0efdec49e88a10cd9ac1a448d5c1de48
asiafrank <zhangxfdev@gmail.com>
2021-01-13 10:05:08 +0800

    题目：股票买卖问题

---
+++ java/algorithm/src/main/java/labuladong/dp/C1MaxProfit.java
@@ -0,0 +1,22 @@
+package labuladong.dp;
+
+/**
+ * 股票买卖问题
+ *
+ * @author zhangxiaofan 2020/12/22-15:01
+ */
+public class C1MaxProfit {
+
+    /**
+     * LeetCode 121. 买卖股票的最佳时机
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
+     * 注意：你不能在买入股票前卖出股票。
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
