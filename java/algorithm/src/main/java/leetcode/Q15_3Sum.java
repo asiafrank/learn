7d0c78170be550c8b3402f56f91cac8ca1c62007
asiafrank <zhangxfdev@gmail.com>
2021-01-13 10:00:30 +0800

    3Sum 尝试

--- java/algorithm/src/main/java/leetcode/Q15_3Sum.java
+++ java/algorithm/src/main/java/leetcode/Q15_3Sum.java
@@ -1,11 +1,10 @@
 package leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 三和问题
  * medium
- * TODO:
  * 1. 先会求2元组
  *   - 排序
  *   - 左右指针收集答案
@@ -13,7 +12,43 @@ import java.util.List;
  * https://leetcode-cn.com/problems/3sum/
  */
 public class Q15_3Sum {
-    public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+    /**
+     * 选定一个数，然后求二元组
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 3)
+            return list;
+
+        Arrays.sort(nums);
+
+        // TODO: 去重问题
+        for (int i = 2; i < nums.length; i++) {
+            int pick = nums[i]; // 1. 选定一个数
+
+            // 2. 求二元组
+            int l = 0;
+            int r = i - 1;
+            int target = -pick;
+            while (l != r) {
+                int sum = nums[l] + nums[r];
+                if (sum > target) {
+                    r--;
+                } else if (sum < target) { // 证明太小了，l++
+                    l++;
+                } else if (sum == target) { // 收集答案
+                    list.add(Arrays.asList(nums[l], nums[r], pick));
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> list = threeSum(nums);
+        System.out.println(list);
     }
 }
